b47d7af398495ac0397acc34c5413271c8f3fcf3
lksbks95 <lks.bks@gmail.com>
2025-06-29 11:15:14 -0300

    Create __init__.py

---
+++ backend/src/models/__init__.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Inicializa a instância da base de dados.
+# Este objeto 'db' será importado por todos os outros modelos.
+db = SQLAlchemy()
+
+# Importa todos os modelos aqui para garantir que eles sejam registados
+# com o SQLAlchemy antes que a aplicação tente usá-los.
+# A ordem aqui não importa, pois o Python irá resolver as dependências.
+from .user import User, Department
+from .conversation import Conversation
+from .contact import Contact
+from .activity_log import ActivityLog
