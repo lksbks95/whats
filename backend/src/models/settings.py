2a6c530aa7071a8dd008b2f28327fd14e5770ce1
lksbks95 <lks.bks@gmail.com>
2025-07-07 14:57:27 -0300

    Delete backend directory

--- backend/src/models/settings.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-class Setting(db.Model):
-    __tablename__ = 'settings'
-    key = db.Column(db.String(50), primary_key=True)
-    value = db.Column(db.String(255), nullable=True)
-
-    def __repr__(self):
-        return f'<Setting {self.key}>'
